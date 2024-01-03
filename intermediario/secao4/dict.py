c62b2b850465e9033375aa24de7c2022e140230e
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-01-02 23:03:03 -0300

    novos scripts

---
+++ intermediario/secao4/dict.py
@@ -0,0 +1,16 @@
+#inicio do estudo de dicionarios, dicts
+
+pessoa = {
+    'nome' : 'luiz otavio',
+    'sobrenome' : 'miranda',
+    'idade' : 18,
+    'altura': 1.8,
+    'endereco' : [
+        {'rua': 'tal tal', 'numero': 123},
+        {'rua': 'outra rua', 'numero': 321}
+    ],
+}
+
+pessoateste = {}
+pessoateste['nome'] = 'Breno'
+pessoateste['Breno']
