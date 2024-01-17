a5cb4e993edd617ed1917ace48155fb45fa58bf7
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-01-17 13:56:29 -0300

    flappybird

--- FlappyBird/Main.py
+++ FlappyBird/Main.py
@@ -1,3 +1,23 @@
-import pygames as pg
+import pygame
+import os
+import random
 
-nome = pg.app 
+TELA_LARGURA = 500
+TELA_ALTURA = 800
+
+IMAGEM_CANO = pygame.transform.scale2x(
+
+    pygame.image.load(os.path.join('imgs', 'base.png')))
+IMAGEM_BACKGROUND = pygame.transform.scale2x(
+    pygame.image.load(os.path.join('imgs', 'bg.png')))
+IMAGEM_PASSARO = [
+    pygame.transform.scale2x(pygame.image.load(
+        os.path.join('imgs', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(
+        os.path.join('imgs', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(
+        os.path.join('imgs', 'bird3.png')))
+]
+
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont('arial', 50)
